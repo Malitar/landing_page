607502b7a8a665f7efd11b2eb8927b242a622604
Alexander Symanzik <Alexander.Symanzik@provitex.de>
2021-11-22 12:36:15 +0100

    html skeleton

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+     <meta charset="utf-8" />
+     <title>App</title>
+     <meta name="viewport" content="width=device-width, initial-scale=1" />
+     <link rel="stylesheet" href="./public/src/all.css" />
+</head>
+
+<body>
+     <h1>Hello!</h1>
+</body>
+
+</html>
