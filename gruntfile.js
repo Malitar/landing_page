84ab9b490118d67fe6dda834ab5a696aa08d0459
Alexander Symanzik <Alexander.Symanzik@provitex.de>
2021-11-22 15:43:47 +0100

    Adjust gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -1,28 +1,54 @@
-const sass= require('node-sass');
+const sass = require('node-sass');
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
      // Project configuration.
      grunt.initConfig({
-       pkg: grunt.file.readJSON('package.json'),
-       sass:{
-		options: {
-            outputStyle: 'compressed',
-            implementation: sass //and this part
-		},
-            dist: {
-               files: {
-                    'public/src/all.css' : 'public/src/scss/main.scss'
-               }
-            }
-       }
+          pkg: grunt.file.readJSON('package.json'),
+          paths: {
+               public: "<%= paths.public %>public",
+               src: "<%= paths.src %>src/",
+               sass: "<%= paths.public %>scss/",
+          },
+
+
+          watch: {
+               options: {
+                    livereload: true,
+               },
+               sass: {
+                  files: [
+                    'public/src/scss/*.scss'
+                    //'public/src/scss/main.scss'
+                    ],  
+                    tasks: ['sass']
+               },  
+          },
+
+          sass: {
+               options: {
+                    outputStyle: 'compressed',
+                    implementation: sass
+               },
+               dist: {
+                    files: {
+                         'public/src/all.css': 'public/src/scss/*.scss'
+                    }
+               },
+               
+          },
+
      });
-   
+
      // Load the plugin that provides the "uglify" task.
      grunt.loadNpmTasks('grunt-contrib-uglify');
      grunt.loadNpmTasks('grunt-sass');
-   
+     grunt.loadNpmTasks('grunt-contrib-watch');
+
+     grunt.registerTask("myTask", ["sass"]);
+     grunt.registerTask("dev", ['watch']);
+
      // Default task(s).
      grunt.registerTask('default', ['sass']);
-   
-   };
+
+};
